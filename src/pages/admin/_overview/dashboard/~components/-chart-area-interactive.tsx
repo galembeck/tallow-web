d52a3f0167aae0b88c5c2684b67b68f899d2d3ad
Pedro Galembeck <galembeckpedro@gmail.com>
2026-03-29 21:02:38 -0300

    fix: correct lint issues/warnings on 'chart-line-interactive' component

--- src/pages/admin/_overview/dashboard/~components/-chart-area-interactive.tsx
+++ src/pages/admin/_overview/dashboard/~components/-chart-area-interactive.tsx
@@ -172,7 +172,7 @@ export function ChartLineInteractive() {
       <CardContent className="px-2 sm:p-6">
         <ChartContainer
           config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
+          className="aspect-auto h-62.5 w-full"
         >
           <LineChart
             accessibilityLayer
@@ -200,7 +200,7 @@ export function ChartLineInteractive() {
             <ChartTooltip
               content={
                 <ChartTooltipContent
-                  className="w-[150px]"
+                  className="w-37.5"
                   nameKey="views"
                   labelFormatter={(value) => {
                     return new Date(value).toLocaleDateString("en-US", {
